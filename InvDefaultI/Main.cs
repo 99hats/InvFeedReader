b5819b75ab7358393a3ab40f64bc97b3ba7d48f3
Kevin Smith <kevin@mcweekly.com>
2017-08-18 09:56:25 -0700

    added ios

---
+++ InvDefaultI/Main.cs
@@ -0,0 +1,12 @@
+﻿using UIKit;
+
+namespace InvDefaultI
+{
+    public sealed class Program
+    {
+        static void Main(string[] args)
+        {
+            Inv.iOSShell.Run(InvDefault.Shell.Install);
+        }
+    }
+}
