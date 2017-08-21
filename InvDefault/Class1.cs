78f209a3ab2242a38736d6b620d214241751bbc9
Kevin Smith <kevin@mcweekly.com>
2017-08-20 23:55:53 -0700

    works!

--- InvDefault/Class1.cs
+++ InvDefault/Class1.cs
@@ -1,5 +1,6 @@
 ﻿using Inv;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
@@ -13,32 +14,41 @@ namespace InvDefault
 
         public static void Install(Inv.Application application)
         {
-
             _application = application;
 
             application.Title = "My Project";
 
-            var Surface = application.Window.NewSurface();
-            Surface.Background.Colour = Colour.WhiteSmoke;
+            var surface = application.Window.NewSurface();
+            surface.Background.Colour = Colour.WhiteSmoke;
 
             var cache = new Dictionary<int, Panel>();
+            cache.Clear();
 
-            var flow = Surface.NewFlow();
-            flow.Margin.Set(24);
+            var flow = surface.NewFlow();
+            //flow.Margin.Set(24);
             var section = flow.AddSection();
 
-            var headerLabel = Surface.NewLabel();
+            var headerLabel = surface.NewLabel();
             headerLabel.Text = "Section 1";
             section.SetHeader(headerLabel);
 
-            // https://mcweekly-app.firebaseio.com/v6/basic.json?orderBy=%22starttime%22&limitToLast=10&endAt=%22ZZZ%22&startAt=%22%22
-            var broker = application.Web.NewBroker("https://mcweekly-app.firebaseio.com/v6/");
+            IList<dynamic> items = new List<dynamic>();
 
-            var json = broker.GetTextJsonObject<Dictionary<string, dynamic>>(
-                "basic.json?orderBy=%22starttime%22&limitToLast=20&endAt=%22ZZZ%22&startAt=%22%22");
+            surface.Window.RunTask(Thread =>
+            {
+                // https://mcweekly-app.firebaseio.com/v6/basic.json?orderBy=%22starttime%22&limitToLast=10&endAt=%22ZZZ%22&startAt=%22%22
+                var broker = application.Web.NewBroker("https://mcweekly-app.firebaseio.com/v6/");
+
+                var json = broker.GetTextJsonObject<Dictionary<string, dynamic>>(
+                    "basic.json?orderBy=%22starttime%22&limitToLast=60&endAt=%22ZZZ%22&startAt=%22%22");
+                items = json.Values.ToList();
+                items.Reverse();
+
+                Thread.Post(()=> section.SetItemCount(items.Count));
+            });
 
             Debug.WriteLine($"Width: {application.Window.Width}");
-            var items = json.Values.ToList();
+            
 
             section.ItemQuery += i =>
             {
@@ -47,20 +57,23 @@ namespace InvDefault
                 string uri = items[i].preview_url;
                 Debug.WriteLine(uri);
 
-                var graphic = new WebGraphic(_application, Surface, uri);
+                var graphic = new WebGraphic(_application, surface, uri);
 
                 var max = 200;
                 var size = Math.Max(i, 5);
                 size = Math.Min(size, 50);
-                graphic.Size.SetWidth(300);
+                //graphic.Size.SetWidth(300);
+                graphic.Size.SetWidth(surface.Window.Width);
+                //graphic.Alignment.TopStretch();
+                //graphic.Alignment.StretchCenter();
 
-                var lbl = Surface.NewLabel();
+                var lbl = surface.NewLabel();
                 lbl.LineWrapping = true;
                 lbl.Text = items[i].title;
                 lbl.Margin.Set(16);
                 lbl.Size.AutoHeight();
                 lbl.Size.AutoMaximumWidth();
-                var panel = Surface.NewTable();
+                var panel = surface.NewTable();
                 var gRow = panel.AddRow();
                 gRow.Star();
 
@@ -73,30 +86,35 @@ namespace InvDefault
                 panel.GetCell(0, 0).Content = graphic;
                 panel.GetCell(0, 1).Content = lbl;
 
-                panel.Margin.Set(8, 4, 32, 4);
-                panel.Padding.Set(8);
+                //panel.Margin.Set(8, 4, 32, 4);
+                //panel.Padding.Set(8);
 
                 panel.AdjustEvent += () => Debug.WriteLine($"Panel: {panel.Surface.Window.Width}");
 
+                panel.Border.Set(0,0,0,2);
+                panel.Border.Colour = Colour.DimGray;
 
+                var button = surface.NewButton();
+                button.Content = panel;
+                button.SingleTapEvent += () => Debug.WriteLine("Button Pressed");
 
-                panel.Background.Colour = Colour.Beige;
+                cache.Add(i, button);
 
-                cache.Add(i, panel);
-
-                return panel;
+                return button;
             };
 
-            section.SetItemCount(json.Count);
-            var dock = Surface.NewHorizontalDock();
-            dock.Size.Auto();
-            dock.AddClient(flow);
+            section.SetItemCount(0);
 
-            Surface.Content = dock;
+            //var dock = surface.NewHorizontalDock();
+            //dock.Size.Auto();
+            //dock.AddClient(flow);
 
-            application.Window.Transition(Surface);
+            surface.Content = flow;
+
+            application.Window.Transition(surface);
         }
     }
+
     public class WebGraphic : Inv.Mimic<Graphic>
     {
         public WebGraphic(Application _application, Surface surface, string uri)
@@ -105,59 +123,28 @@ namespace InvDefault
 
             this.Base.Image = Inv.Default.Resources.Images.Logo; ; // put loading image here
 
-            surface.Window.RunTask(Thread =>
-            {
-                using (var download = _application.Web.GetDownload(new Uri(uri)))
-                using (var memoryStream = new MemoryStream((int)download.Length))
+            if (!String.IsNullOrEmpty(uri))
+                surface.Window.RunTask(Thread =>
                 {
-                    download.Stream.CopyTo(memoryStream);
 
-                    memoryStream.Flush();
+                    Debug.WriteLine($"Loading: {uri}");
+                    //Thread.Sleep(TimeSpan.FromMilliseconds(500));
 
-                    var image = new Inv.Image(memoryStream.ToArray(), ".png");
+                    using (var download = _application.Web.GetDownload(new Uri(uri)))
+                    using (var memoryStream = new MemoryStream((int)download.Length))
+                    {
+                        download.Stream.CopyTo(memoryStream);
 
-                    Thread.Post(() => this.Base.Image = image);
-                }
-            });
+                        memoryStream.Flush();
+
+                        var image = new Inv.Image(memoryStream.ToArray(), ".png");
+
+                        Thread.Post(() => { this.Base.Image = image; });
+                    }
+                });
         }
 
         public Size Size => Base.Size;
+        public Alignment Alignment => Base.Alignment;
     }
-
-    //public class WebGraphic : Inv.Mimic<Graphic>
-    //{
-    //    private readonly Application _application;
-    //    private readonly string _uri;
-
-    //    public WebGraphic(Application application, Surface surface, string uri)
-    //    {
-    //        this.Base = surface.NewGraphic();
-    //        this.Size = this.Base.Size;
-
-    //        _application = application;
-    //        _uri = uri;
-
-    //        this.Base.Image = Inv.Default.Resources.Images.Logo; ; // put loading image here
-
-    //        _application.Window.RunTask(GetImage(_uri));
-    //    }
-
-    //    public Size Size { get; set; }
-
-    //    public Action<WindowThread> GetImage(string uri)
-    //    {
-    //        using (var download = _application.Web.GetDownload(new Uri(uri)))
-    //        using (var memoryStream = new MemoryStream((int)download.Length))
-    //        {
-    //            download.Stream.CopyTo(memoryStream);
-
-    //            memoryStream.Flush();
-
-    //            var image = new Inv.Image(memoryStream.ToArray(), ".png");
-
-    //            this.Base.Image = image;
-    //        }
-    //        return null;
-    //    }
-    //}
 }
