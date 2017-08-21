05381bd3c4d339b77fafa15e6c1bafea1a00c5be
Kevin Smith <kevin@mcweekly.com>
2017-08-20 21:46:39 -0700

    bump

--- InvDefault/Class1.cs
+++ InvDefault/Class1.cs
@@ -1,9 +1,9 @@
-﻿using System;
+﻿using Inv;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Threading.Tasks;
-using Inv;
+using System.Linq;
 
 namespace InvDefault
 {
@@ -19,82 +19,65 @@ namespace InvDefault
             var cache = new Dictionary<int, Panel>();
 
             var flow = Surface.NewFlow();
-            //flow.Size.AutoWidth();
             flow.Margin.Set(24);
             var section = flow.AddSection();
-
-
 
             var headerLabel = Surface.NewLabel();
             headerLabel.Text = "Section 1";
             section.SetHeader(headerLabel);
 
+            // https://mcweekly-app.firebaseio.com/v6/basic.json?orderBy=%22starttime%22&limitToLast=10&endAt=%22ZZZ%22&startAt=%22%22
+            var broker = Application.Web.NewBroker("https://mcweekly-app.firebaseio.com/v6/");
 
+            var json = broker.GetTextJsonObject<Dictionary<string, dynamic>>(
+                "basic.json?orderBy=%22starttime%22&limitToLast=10&endAt=%22ZZZ%22&startAt=%22%22");
 
             Debug.WriteLine($"Width: {Application.Window.Width}");
+            var items = json.Values.ToList();
 
             section.ItemQuery += i =>
             {
-
                 if (cache.ContainsKey(i)) return cache[i];
 
-                if (section.ItemCount % 60 == 0)
-                    section.SetItemCount(section.ItemCount + 60);
+                string uri = items[i].preview_url;
+                Debug.WriteLine(uri);
 
-                //var graphic = Surface.NewGraphic();
-                //graphic.Image = Inv.Default.Resources.Images.Logo;
-                var uri = $"https://dummyimage.com/300x{200 + i}/000/fff";
                 var graphic = new WebGraphic(Application, Surface, uri);
 
                 var max = 200;
                 var size = Math.Max(i, 5);
                 size = Math.Min(size, 50);
-                graphic.Size.SetHeight(size);
-
+                graphic.Size.SetWidth(300);
 
                 var lbl = Surface.NewLabel();
                 lbl.LineWrapping = true;
-                lbl.Text = $" Foo {i}";
+                lbl.Text = items[i].title;
                 lbl.Margin.Set(16);
                 lbl.Size.AutoHeight();
                 lbl.Size.AutoMaximumWidth();
-                //lbl.Size.SetMaximumWidth(300);
-                //lbl.Size.AutoWidth();
-                //lbl.Size.AutoMaximumHeight();
-
-
-                //var panel = Surface.NewTable();
-
-
                 var panel = Surface.NewTable();
-                var row = panel.AddRow();
-                row.Auto();
+                var gRow = panel.AddRow();
+                gRow.Star();
 
-                var gCol = panel.AddColumn();
-                gCol.Fixed(124);
+                var tRow = panel.AddRow();
+                tRow.Auto();
 
                 var tCol = panel.AddColumn();
                 tCol.Star();
 
                 panel.GetCell(0, 0).Content = graphic;
-                panel.GetCell(1, 0).Content = lbl;
+                panel.GetCell(0, 1).Content = lbl;
 
-                    panel.Size.Auto();
-                //panel.Size.AutoWidth();
-                panel.Margin.Set(8,4,32,4);
+                panel.Margin.Set(8, 4, 32, 4);
                 panel.Padding.Set(8);
 
                 panel.AdjustEvent += () => Debug.WriteLine($"Panel: {panel.Surface.Window.Width}");
 
-                //panel.AddPanel(graphic);
-
-                
 
                 for (int j = 0; j < i; j++)
                 {
                     lbl.Text += " bar";
                 }
-                //panel.AddPanel(lbl);
 
                 panel.Background.Colour = Colour.Beige;
 
@@ -103,100 +86,16 @@ namespace InvDefault
                 return panel;
             };
 
-            section.SetItemCount(60);
-
-            //var section2 = flow.AddSection();
-
-            //var header2Label = Surface.NewLabel();
-            //header2Label.Text = "Section 2";
-            //section2.SetHeader(header2Label);
-            ////section2.ItemQuery += i =>
-            //{
-
-            //    if (cache.ContainsKey(i*10000)) return cache[i*10000];
-
-            //    //if (section.ItemCount - i == 20)
-            //    //    section.SetItemCount(section.ItemCount + 20);
-
-            //    var graphic = Surface.NewGraphic();
-            //    graphic.Image = Inv.Default.Resources.Images.Logo;
-            //    var max = 200;
-            //    var size = Math.Max(i, 5);
-            //    size = Math.Min(size, 50);
-            //    graphic.Size.SetHeight(size);
-
-            //    var panel = Surface.NewHorizontalStack();
-            //    panel.Size.AutoMaximumHeight();
-            //    panel.Size.SetWidth(Application.Window.Width - 16);
-
-            //    panel.AddPanel(graphic);
-
-            //    var lbl = Surface.NewLabel();
-            //    lbl.LineWrapping = true;
-            //    lbl.Text = $" Foo {i}";
-            //    //lbl.Size.SetMaximumWidth(300);
-            //    lbl.Size.AutoMaximumHeight();
-
-            //    for (int j = 0; j < i; j++)
-            //    {
-            //        lbl.Text += " baz";
-            //    }
-            //    panel.AddPanel(lbl);
-            //    panel.Background.Colour = Colour.Beige;
-
-            //    cache.Add(i*10000, panel);
-
-            //    return panel;
-            //};
-            //section2.SetItemCount(60);
-
-
-
+            section.SetItemCount(json.Count);
             var dock = Surface.NewHorizontalDock();
             dock.Size.Auto();
             dock.AddClient(flow);
 
-
-            //dock.AdjustEvent += () =>
-            //{
-            //    flow.Size.SetWidth(dock.Surface.Window.Width - 124);
-            //    Debug.WriteLine($"Surface: {Surface.Content.Surface.Window.Width} Dock: {dock.Surface.Window.Width} Flow: {flow.Surface.Window.Width} ");
-            //};
-
-            //flow.AdjustEvent += () => Debug.WriteLine($"Flow: {flow.Surface.Window.Width}");
-
-        Surface.Content = dock;
-
-
-
-
-
-            //var panel = Surface.NewHorizontalStack();
-
-
-            //var graphic = Surface.NewGraphic();
-            //graphic.Image = Inv.Default.Resources.Images.Logo;
-            //graphic.Size.SetMaximumHeight(100);
-
-            //panel.AddPanel(graphic);
-
-            //var Label = Surface.NewLabel();
-            //Label.Alignment.Center();
-            //Label.Font.Size = 20;
-            //Label.Text = $"Invention";
-            //panel.AddPanel(Label);
-
-            //panel.Size.SetMaximumHeight(100);
-
-            //Surface.Content = panel;
-
-
+            Surface.Content = dock;
 
             Application.Window.Transition(Surface);
         }
     }
-
-    
 
     public class WebGraphic : Inv.Mimic<Graphic>
     {
@@ -211,22 +110,15 @@ namespace InvDefault
             _application = application;
             _uri = uri;
 
-            
-            Init();
+            this.Base.Image = Inv.Default.Resources.Images.Logo; ; // put loading image here
+
+            _application.Window.RunTask(GetImage(_uri));
         }
 
         public Size Size { get; set; }
 
-        private async void Init()
+        public Action<WindowThread> GetImage(string uri)
         {
-            this.Base.Image = null; // put loading image here
-            this.Base.Image = await GetImage(_uri);
-        }
-
-        public async Task<Inv.Image> GetImage(string uri)
-        {
-
-
             using (var download = _application.Web.GetDownload(new Uri(uri)))
             using (var memoryStream = new MemoryStream((int)download.Length))
             {
@@ -236,8 +128,9 @@ namespace InvDefault
 
                 var image = new Inv.Image(memoryStream.ToArray(), ".png");
 
-                return image;
+                this.Base.Image = image;
             }
+            return null;
         }
     }
 }
