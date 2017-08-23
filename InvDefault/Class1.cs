69d91dad63c9fc9e5e1908be9477b89a3b64c579
Kevin Smith <kevin@mcweekly.com>
2017-08-22 22:55:08 -0700

    yeah, disk caching images... dang

--- InvDefault/Class1.cs
+++ InvDefault/Class1.cs
@@ -5,6 +5,10 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace InvDefault
 {
@@ -12,36 +16,35 @@ namespace InvDefault
     {
         private static Application _application;
 
+        private static Dictionary<string, bool> _loadingManager = new Dictionary<string, bool>();
+
         public static void Install(Inv.Application application)
         {
             _application = application;
-
             _application.Title = "News Feed";
 
-
             // main surface
-            var surface = application.Window.NewSurface();
-            surface.Background.Colour = Colour.LightGray;
-
+            var mainSurface = application.Window.NewSurface();
+            mainSurface.Background.Colour = Colour.LightGray;
 
             // sub surface
-            Surface sf2;
+            Surface articleSurface;
 
             // grap api key
             string apiKey = System.Text.Encoding.UTF8.GetString(Inv.Default.Resources.Text.Newsapi.GetBuffer());
 
             // main listview
-            var flow = surface.NewFlow();
-            surface.Content = flow;
-            _application.Window.Transition(surface).Fade();
+            var flow = mainSurface.NewFlow();
+            mainSurface.Content = flow;
+            _application.Window.Transition(mainSurface).Fade();
 
             // section
             var section = flow.AddSection();
-            var headerLabel = surface.NewLabel();
+            var headerLabel = mainSurface.NewLabel();
             headerLabel.Text = "Tech Crunch";
             headerLabel.JustifyCenter();
             headerLabel.Alignment.TopStretch();
-            headerLabel.Padding.Set(0, (surface.Window.Height / 3), 0, 4);
+            headerLabel.Padding.Set(0, (mainSurface.Window.Height / 3), 0, 4);
             headerLabel.Background.Colour = Colour.DodgerBlue;
             headerLabel.Font.Colour = Colour.White;
             headerLabel.Font.Size = 32;
@@ -51,8 +54,11 @@ namespace InvDefault
             IList<Article> items = new List<Article>();
 
 
+            //var bgQueue = new BackgroundQueue();
+            //bgQueue.QueueTask(() => LongRunningTask());
+
             // fetch feed
-            surface.Window.RunTask(thread =>
+            mainSurface.Window.RunTask(thread =>
             {
                 var broker = application.Web.NewBroker("https://newsapi.org/v1/");
                 var json = broker.GetTextJsonObject<FeedObject>(@"articles?source=techcrunch&sortBy=latest&apiKey=" +
@@ -72,20 +78,13 @@ namespace InvDefault
 
                 var article = items[i];
 
-                var cellPanel = new FeedItemPanel(_application, surface, article);
+                var cellPanel = new FeedItemPanel(_application, mainSurface, article);
                 cache[i] = cellPanel;
 
-                cellPanel.SingleTapEvent += () =>
+                void OnCellPanelOnSingleTapEvent()
                 {
-
-
-                    
-                    //application.Web.Launch( new Uri(item.url));
-
-
-
-                    sf2 = _application.Window.NewSurface();
-                    var browser = sf2.NewBrowser();
+                    articleSurface = _application.Window.NewSurface();
+                    var browser = articleSurface.NewBrowser();
 
                     var uriString = @"https://mercury.postlight.com/amp?url=" + article.url;
 
@@ -93,152 +92,77 @@ namespace InvDefault
                     {
                         var html = htmlCache[article.url];
                         browser.LoadHtml(html);
-                        sf2.Content = browser;
+                        articleSurface.Content = browser;
                     }
                     else
                     {
-
                         var uri2 = new Uri(uriString);
                         Debug.WriteLine(uri2.AbsoluteUri);
                         browser.LoadUri(uri2);
-
-                        sf2.Content = browser;
+                        articleSurface.Content = browser;
                     }
 
                     // preload next webpage
                     if (i < section.ItemCount - 1)
                     {
-
-
-
-                        surface.Window.RunTask(Thread =>
+                        mainSurface.Window.RunTask(Thread =>
                         {
                             var nextItem = items[i + 1];
                             var key = @"https://mercury.postlight.com/amp?url=" + nextItem.url;
                             var uri3 = new Uri(key);
                             var broker = _application.Web.NewBroker($"{uri3.Scheme}://{uri3.DnsSafeHost}");
-
                             var html = broker.GetPlainText(uri3.PathAndQuery);
 
-                            Thread.Post(() =>
-                            {
-                                htmlCache[nextItem.url] = html;
-                            });
+                            Thread.Post(() => { htmlCache[nextItem.url] = html; });
                         });
                     }
 
-                    _application.Window.Transition(sf2).CarouselNext();
-                    sf2.GestureBackwardEvent += () => { _application.Window.Transition(surface).CarouselBack(); };
-                };
+                    _application.Window.Transition(articleSurface).CarouselNext();
+                    articleSurface.GestureBackwardEvent += () =>
+                    {
+                        _application.Window.Transition(mainSurface).CarouselBack();
+                    };
+                }
+
+                cellPanel.SingleTapEvent += OnCellPanelOnSingleTapEvent;
 
                 return cellPanel;
 
-                //string uri = article.urlToImage ?? ""; // in case there's no image
-
-
-
-
-                //var graphic = new WebGraphic(_application, surface, uri);
-                //graphic.Size.SetWidth(surface.Window.Width);
-
-
-                //var lbl = surface.NewLabel();
-                //lbl.LineWrapping = true;
-                //lbl.Text = article.title;
-                //lbl.Margin.Set(16);
-                //lbl.Size.AutoHeight();
-                //lbl.Size.AutoMaximumWidth();
-                //lbl.Font.Size = 18;
-                //var panel = surface.NewTable();
-                //var gRow = panel.AddRow();
-                //gRow.Star();
-
-                //var tRow = panel.AddRow();
-                //tRow.Auto();
-
-                //var tCol = panel.AddColumn();
-                //tCol.Star();
-
-                //panel.GetCell(0, 0).Content = graphic;
-                //panel.GetCell(0, 1).Content = lbl;
-
-                //panel.AdjustEvent += () => Debug.WriteLine($"Panel: {panel.Surface.Window.Width}");
-
-                //Surface sf2;
-
-                //var button = surface.NewButton();
-                //button.Background.Colour = Colour.WhiteSmoke;
-                //button.Margin.Set(8, 16, 8, 8);
-                //button.Content = panel;
-                //button.SingleTapEvent += () =>
-                //{
-
-                //    //application.Web.Launch( new Uri(item.url));
-
-
-
-                //    sf2 = _application.Window.NewSurface();
-                //    var browser = sf2.NewBrowser();
-
-                //    var uriString = @"https://mercury.postlight.com/amp?url=" + article.url;
-
-                //    if (htmlCache.ContainsKey(article.url))
-                //    {
-                //        var html = htmlCache[article.url];
-                //        browser.LoadHtml(html);
-                //        sf2.Content = browser;
-                //    }
-                //    else
-                //    {
-
-                //        var uri2 = new Uri(uriString);
-                //        Debug.WriteLine(uri2.AbsoluteUri);
-                //        browser.LoadUri(uri2);
-
-                //        sf2.Content = browser;
-
-
-
-
-
-                //    }
-
-                //    // preload next webpage
-                //    if (i < section.ItemCount - 1)
-                //    {
-
-
-
-                //        surface.Window.RunTask(Thread =>
-                //        {
-                //            var nextItem = items[i + 1];
-                //            var key = @"https://mercury.postlight.com/amp?url=" + nextItem.url;
-                //            var uri3 = new Uri(key);
-                //            var broker = _application.Web.NewBroker($"{uri3.Scheme}://{uri3.DnsSafeHost}");
-
-                //            var html = broker.GetPlainText(uri3.PathAndQuery);
-
-                //            Thread.Post(() =>
-                //            {
-                //                htmlCache[nextItem.url] = html;
-                //            });
-                //        });
-                //    }
-
-                //    _application.Window.Transition(sf2).CarouselNext();
-                //    sf2.GestureBackwardEvent += () => { _application.Window.Transition(surface).CarouselBack(); };
-                //};
-
-
-                //cache[i] = button;
-
-                //return button;
             };
+        }
+
+        //private static void LongRunningTask()
+        //{
+        //    Task.Delay(5000);
+        //    Debug.WriteLine("LongRunningTask ran");
+        //}
+
+        /// <summary>
+        /// Returns a MD5 hash as a string
+        /// </summary>
+        /// <param name="TextToHash">String to be hashed.</param>
+        /// <returns>Hash as string.</returns>
+        public static String GetMD5Hash(String TextToHash)
+        {
+            //Check wether data was passed
+            if ((TextToHash == null) || (TextToHash.Length == 0))
+            {
+                return String.Empty;
+            }
+
+            //Calculate MD5 hash. This requires that the string is splitted into a byte[].
+            MD5 md5 = new MD5CryptoServiceProvider();
+            byte[] textToHash = Encoding.Default.GetBytes(TextToHash);
+            byte[] result = md5.ComputeHash(textToHash);
+
+            //Convert result back to string.
+            return System.BitConverter.ToString(result);
         }
 
     }
 
     #region models
+    // modeled using json2csharp.net
         public class Article
         {
             public string author { get; set; }
@@ -306,8 +230,6 @@ namespace InvDefault
             add { this.Base.SingleTapEvent += value; }
             remove { this.Base.SingleTapEvent -= value; }
         }
-
-
     }
 
         public class WebGraphic : Inv.Mimic<Graphic>
@@ -315,23 +237,38 @@ namespace InvDefault
             public WebGraphic(Application _application, Surface surface, string uri)
             {
                 this.Base = surface.NewGraphic();
-
                 this.Base.Image = Inv.Default.Resources.Images.Placeholder; // put loading image here
 
-                if (!String.IsNullOrEmpty(uri))
+            var cacheFileName = Shell.GetMD5Hash(uri) +".jpg";
+            var files = _application.Directory.RootFolder.GetFiles("*.jpg");
+
+            Debug.WriteLine($"cacheFileName: {cacheFileName}");
+            var cache = files.FirstOrDefault(x => x.Name == cacheFileName);
+            if (cache !=null) 
+            {
+                //var cache = _application.Directory.RootFolder.NewFile(cacheFileName);
+                var thebytes = cache.ReadAllBytes();
+                var imagee2 = new Inv.Image(thebytes, ".jpg");
+                this.Base.Image = imagee2;
+                return;
+
+            }
+
+            if (!String.IsNullOrEmpty(uri))
                     surface.Window.RunTask(Thread =>
                     {
-
-
                         using (var download = _application.Web.GetDownload(new Uri(uri)))
 
-                        using (var memoryStream = new MemoryStream((int) download.Length))
+                        using (var memoryStream = new MemoryStream((int)download.Length))
                         {
+                            var cacheFile = _application.Directory.RootFolder.NewFile(cacheFileName);
+
                             download.Stream.CopyTo(memoryStream);
                             memoryStream.Flush();
 
-                            var image = new Inv.Image(memoryStream.ToArray(), ".jpg");
+                            cacheFile.WriteAllBytes(memoryStream.ToArray());
 
+                            var image = new Inv.Image(memoryStream.ToArray(), ".jpg");
                             Thread.Post(() =>
                             {
                                 var FadeInAnimation = surface.NewAnimation();
@@ -346,5 +283,38 @@ namespace InvDefault
             public Size Size => Base.Size;
             public Alignment Alignment => Base.Alignment;
         }
-        #endregion
+
+    public class BackgroundQueue
+    {
+        private Task previousTask = Task.FromResult(true);
+        private object key = new object();
+        public Task QueueTask(Action action)
+        {
+            lock (key)
+            {
+                previousTask = previousTask.ContinueWith(t => action()
+                    , CancellationToken.None
+                    , TaskContinuationOptions.None
+                    , TaskScheduler.Default);
+                return previousTask;
+            }
+        }
+
+        public Task<T> QueueTask<T>(Func<T> work)
+        {
+            lock (key)
+            {
+                var task = previousTask.ContinueWith(t => work()
+                    , CancellationToken.None
+                    , TaskContinuationOptions.None
+                    , TaskScheduler.Default);
+                previousTask = task;
+                return task;
+            }
+        }
+    }
+
+
+
+    #endregion helpers
 }
